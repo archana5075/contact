239227567ed28c9302d9fe98058e1bafa56b0592
archanaPotnis <potnis.archana@gmail.com>
2014-10-16 18:40:07 -0700

    updated README

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,53 +4,45 @@
 
 angular.module('myApp.controllers', [])
   .controller('LandingPageController',[function(){
-
-
   }])
-  .controller('ContactListController',['$scope','$firebase','$resource', function($scope, $firebase, $resource){
-
-                $scope.names = [];
-                  var resource = $resource(
+  .controller('ContactListController',['$scope','$http','$firebase','$resource', function($scope, $http, $firebase, $resource){
+                 
+                 var resource = $resource(
                     "https://api.sendhub.com/v1/contacts/?username=6159738279&api_key=a0f2149735eee4c3abb4b9a6eb8097e4d3f096ac",
                     {
                         callback: "JSON_CALLBACK"
                     },
                     {
                         getNames: {
-                            method: "JSONP"
+                                    method: "JSONP"
                                   }
                     }
                 );
 
                  resource.getNames().$promise.then(
-                       function( names ) {
-                          console.log(names);  
-                          $scope.names = names;
+                       function( response ) {
+                          $scope.contactList = response.objects;
+                          //console.log($scope.contactList);  
                          },
                         function( error ) {
                           alert( "Something went wrong!" );
                         }
                     );
 
-      var taskRef = new Firebase('https://sendhub.firebaseio.com/contacts');
-      $scope.contactList = $firebase(taskRef);
-      
-       $scope.newContact = {name:'', number:''};
+    $scope.newContact  = {name:'', number:''};  
+    $scope.addContact = function()
+    { 
+      $scope.contactList.push($scope.newContact);
+    }
 
-        $scope.saveContact =  function(){
-         $scope.contactList.$add($scope.newContact);
-         $scope.newContact = {name:'', number:'' };
-        };
-
-      $scope.sendtextMessage = function(phoneNumber){
+    $scope.sendtextMessage = function(phoneNumber){
         var textMessagesRef = new Firebase('https://sendhub.firebaseio.com/textMessages');
         var textMessages = $firebase(textMessagesRef);
         textMessages.$add({phoneNumber: phoneNumber});
-      }; 
+    };
+                            
+    
+
       
-
-
-
-       
   }]);
  
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,10 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', [])
+.value('version', '0.1')
+.factory("ContactFactory", function($resource) {
+  return $resource("https://api.sendhub.com/v1/contacts/?username=6159738279\&api_key=a0f2149735eee4c3abb4b9a6eb8097e4d3f096ac");
+});
+
+  
